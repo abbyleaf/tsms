5268106dee1f2eb6ded7e69e9333105572932ed3
Daniel Tan <50669678+danieltan2018@users.noreply.github.com>
2021-01-13 14:41:10 +0800

    Create logparser.py

---
+++ logparser.py
@@ -0,0 +1,23 @@
+import json
+
+with open('debug.log', 'r', encoding='UTF8') as logfile:
+    for line in logfile:
+        line = line.strip()
+        if line.startswith('2'):
+            continue
+        line = '{' + line
+        try:
+            data = json.loads(line)
+        except:
+            continue
+        if 'message' in data:
+            data = data['message']
+            try:
+                print(data['from']['first_name'], ':', data['text'])
+            except:
+                print('CONTACT')
+        elif 'callback_query' in data:
+            data = data['callback_query']
+            print(data['from']['first_name'], ':', data['data'])
+        else:
+            print(data)
