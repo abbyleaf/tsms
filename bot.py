349ebd6b8a63d7bfb3837e8d01361c375e88da59
Daniel Tan <50669678+danieltan2018@users.noreply.github.com>
2024-02-20 21:19:56 +0800

    Use cache

--- bot.py
+++ bot.py
@@ -18,6 +18,7 @@ from unidecode import unidecode
 from fuzzywuzzy import fuzz
 from requests import get
 import time
+from cache import titles, songs, chords, scores, mp3, piano
 
 ip = get("https://api.ipify.org").text
 try:
@@ -74,110 +75,6 @@ def loader():
     except:
         with open("users.json", "w+") as userfile:
             users = {}
-
-    global songs
-    songs = {}
-    global titles
-    titles = {}
-    number = re.compile("^\d")
-    for filename in (x.name for x in os.scandir("./books") if x.is_file()):
-        filepath = "./books/" + filename
-        with open(filepath, "r", encoding="UTF8") as book:
-            filename = filename.split(".")
-            bookname = filename[0]
-            print("Loading", bookname)
-            song_number = None
-            song_lyrics = ""
-            for line in book:
-                if number.search(line):
-                    if song_number != None:
-                        song_lyrics = song_lyrics.strip()
-                        songs[song_number] = song_lyrics
-                        song_lyrics = ""
-                    song_lyrics += bookname + " " + line
-                    line = line.split()
-                    song_number = bookname + " " + str(line.pop(0))
-                    song_title = " ".join(line)
-                    titles[song_number] = song_title
-                else:
-                    song_lyrics += line
-            song_lyrics = song_lyrics.strip()
-            songs[song_number] = song_lyrics
-
-    global chords
-    chords = {}
-    number = re.compile("^\d")
-    with open("./media/tsms_chords.txt", "r", encoding="UTF8") as tsms_chords_file:
-        print("Loading Chords")
-        song_number = None
-        song_lyrics = ""
-        for line in tsms_chords_file:
-            if number.search(line):
-                if song_number != None:
-                    song_lyrics = song_lyrics.strip()
-                    chords[song_number] = song_lyrics
-                    song_lyrics = ""
-                song_lyrics += "TSMS " + line
-                line = line.split()
-                song_number = "TSMS " + str(line.pop(0))
-            else:
-                song_lyrics += line
-        song_lyrics = song_lyrics.strip()
-        chords[song_number] = song_lyrics
-    with open("./media/cm_chords.txt", "r", encoding="UTF8") as cm_chords_file:
-        print("Loading CM Chords")
-        song_number = None
-        song_lyrics = ""
-        for line in cm_chords_file:
-            if number.search(line):
-                if song_number != None:
-                    song_lyrics = song_lyrics.strip()
-                    chords[song_number] = song_lyrics
-                    song_lyrics = ""
-                song_lyrics += "CM " + line
-                line = line.split()
-                song_number = "CM " + str(line.pop(0))
-            else:
-                song_lyrics += line
-        song_lyrics = song_lyrics.strip()
-        chords[song_number] = song_lyrics
-
-    global scores
-    scores = {}
-    with open("./media/scores.txt", "r", encoding="UTF8") as scores_file:
-        print("Loading Scores")
-        for line in scores_file:
-            line = line.strip()
-            line = line.split("@")
-            reference = line[1]
-            line = line[0]
-            line = line.split("_")
-            number = line[0]
-            scores.setdefault(number, []).append(reference)
-
-    global mp3
-    mp3 = {}
-    with open("./media/mp3.txt", "r", encoding="UTF8") as mp3_file:
-        print("Loading MP3")
-        for line in mp3_file:
-            line = line.strip()
-            line = line.split("@")
-            reference = line[1]
-            line = line[0]
-            line = line.split("_")
-            number = line[0]
-            mp3.setdefault(number, []).append(reference)
-
-    global piano
-    piano = {}
-    with open("./media/wilds_piano.txt", "r", encoding="UTF8") as piano_file:
-        print("Loading Piano")
-        for line in piano_file:
-            line = line.strip()
-            line = line.split("@")
-            reference = line[1]
-            number = line[0]
-            piano[number] = reference
 
 
 def start(update, context):
